31d52059611f7a8cd2c447ffb81a8e057b87c69a
Dinyar Rabady <dinyar.rabady@cern.ch>
2015-09-10 01:36:52 +0200

    Added script to run ntuplizer.

---
+++ runL1RecoMuonTreeNtuplizer.C
@@ -0,0 +1,8 @@
+{
+  gROOT->ProcessLine(".x ../../L1TriggerDPG/L1Ntuples/macros/initL1Analysis.C");
+  gROOT->ProcessLine(".L GMTChargeAssignmentNtupleizer.C+");
+  gROOT->ProcessLine(
+      "GMTChargeAssignmentNtupleizer macro = "
+      "GMTChargeAssignmentNtupleizer(\"murecotree_list\");");
+  gROOT->ProcessLine("macro.run(-1);");
+}
