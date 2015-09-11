0c7a1c5d6ef8c351df6f3beb52f3de5230cb80ad
Dinyar Rabady <dinyar@rabady.at>
2015-09-11 17:56:26 +0200

    Corrected run script.

--- runL1RecoMuonTreeNtuplizer.C
+++ runL1RecoMuonTreeNtuplizer.C
@@ -1,8 +1,8 @@
 {
-  gROOT->ProcessLine(".x ../L1TriggerDPG/L1Ntuples/macros/initL1Analysis.C");
-  gROOT->ProcessLine(".L GMTChargeAssignmentNtupleizer.C+");
+  gROOT->ProcessLine(".x ../L1Trigger/L1TNtuples/macros/initL1Analysis.C");
+  gROOT->ProcessLine(".L L1RecoMuonTreeNtuplizer.C+");
   gROOT->ProcessLine(
-      "GMTChargeAssignmentNtupleizer macro = "
-      "GMTChargeAssignmentNtupleizer(\"murecotree_list\");");
+      "L1RecoMuonTreeNtuplizer macro = "
+      "L1RecoMuonTreeNtuplizer(\"murecotree_list\");");
   gROOT->ProcessLine("macro.run(-1);");
 }
